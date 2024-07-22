2a770de71e5f69bfa5084f357d60bcdeb4a4366d
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-22 21:12:15 +0530

    Create Student.cs

---
+++ Project1/Project1/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string StudentId { get; set; }
+        public string Major { get; set; }
+
+        public List<Course> courses { get; set; }
+
+        public Student(string name,string id,string major) 
+        {
+            Name = name;
+            StudentId = id;
+            Major = major;
+        }
+
+        public void EntrolInCourse(Course course) 
+        {
+            courses.Add(course);
+        }
+
+        public List <Course> GetCourses() 
+        {
+            return courses;
+        }
+    }
+}

153bb11749ab20559e04fff0f250554cdddf1f4c
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-22 21:19:43 +0530

    Create Department.cs

---
+++ Project1/Project1/Department.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Department
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public List<Course> Courses { get; set; }
+        public List<Professor> Processors { get; set; }
+        public List<Student> Students { get; set; }
+
+        public Department (string Name,string Code) 
+        {
+            this.Name = Name;
+            this.Code = Code;
+        }
+    }
+}

a6cdd900b7c460730e21e71f7cc2d5887f898278
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-22 21:15:27 +0530

    Create Professor.cs

---
+++ Project1/Project1/Professor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Professor
+    {
+        public string Name { get; set; }
+        public string ProfessorID { get; set; }
+        public string Specialization { get; set; }
+
+        public List<Course> courses { get; set; }
+
+        public Professor(string name,string profId,string spec) 
+        {
+            Name = name;
+            ProfessorID = profId;
+            Specialization = spec;
+        }
+
+        public void AddCourse(Course course) 
+        {
+            courses.Add(course);
+        }
+
+        public List <Course> GetCourses() 
+        {
+            return courses;
+        }
+
+
+    }
+}

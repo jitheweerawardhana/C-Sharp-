13a0d554afbd4a8180313bb124598e0bdc9d5ad0
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-22 21:15:10 +0530

    Create University.cs

---
+++ Project1/Project1/University.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class University
+    {
+       public string Name {  get; set; }
+       public string Location { get; set; }
+
+        public List<Department> departments { get; set; }
+
+       public University(String name,String location) 
+        {
+            Name = name;
+            Location = location;
+        }
+
+        public void AddDepartment (Department dep)
+        {
+            departments.Add(dep);
+        }
+
+        public List<Department> GetDepartments() 
+        {
+            return departments;
+        }
+    }
+}

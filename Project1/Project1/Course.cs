1e3924e9062f242167d646d2932be947e8d418b9
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-22 21:20:17 +0530

    Create Course.cs

---
+++ Project1/Project1/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Course
+    {
+        public string Title { get; set; }
+        public string CourseId { get; set; }
+
+        public int Credits { get; set; }
+    }
+}

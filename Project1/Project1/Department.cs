b26db13980d6c224e2c7e1a5c80503a8058b4c60
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-29 21:41:10 +0530

    Update Department.cs

--- Project1/Project1/Department.cs
+++ Project1/Project1/Department.cs
@@ -12,13 +12,52 @@ namespace Project1
         public string Code { get; set; }
 
         public List<Course> Courses { get; set; }
-        public List<Professor> Processors { get; set; }
+        public List<Professor> Professors { get; set; }
         public List<Student> Students { get; set; }
 
         public Department (string Name,string Code) 
         {
             this.Name = Name;
             this.Code = Code;
+            Courses = new List<Course> ();
+            Professors = new List<Professor> ();
+            Students = new List<Student> ();
+
+        }
+
+        public void AddCourses (Course course)
+        {
+            Courses.Add (course);
+        }
+
+        public List<Course> GetCourses()
+        { 
+            return Courses;
+        }
+
+        public void AddProfessors(Professor professor) 
+        {
+            Professors.Add (professor);
+        }
+
+        public List<Professor> GetProfessors() 
+        { 
+            return Professors; 
+        }
+
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+        }
+
+        public List<Student> GetStudents()
+        {
+            return Students;
+        }
+
+        public static implicit operator Department(List<Department> v)
+        {
+            throw new NotImplementedException();
         }
     }
 }

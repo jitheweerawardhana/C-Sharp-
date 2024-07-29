6be9772e9f36f51dbaabfb490968241249d22f58
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-29 21:41:02 +0530

    Update Course.cs

--- Project1/Project1/Course.cs
+++ Project1/Project1/Course.cs
@@ -10,7 +10,33 @@ namespace Project1
     {
         public string Title { get; set; }
         public string CourseId { get; set; }
-
         public int Credits { get; set; }
+
+        public List<Student> Students { get; set; }
+        public List<Professor> Professors { get; set; }
+
+        public Course(String title, String courseID, int credits)
+        {
+            Title = title;
+            CourseId = courseID;
+            Credits = credits;
+            Students = new List<Student>();
+            Professors = new List<Professor>();
+        }
+        public void EnrolStudent(Student student)
+        {
+            Students.Add(student);
+        }
+        public List<Student>GetStudent()
+        { return Students; }
+
+        public void SetProfessor(Professor professor)
+        {
+            Professors.Add(professor);
+        }
+        public List< Professor>GetProfessors()
+            { 
+                 return Professors; 
+        }
     }
 }

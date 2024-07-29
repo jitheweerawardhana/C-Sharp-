a2cb1b57b53c9022b9ef1535ab423bff087e0ed4
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-07-29 21:42:13 +0530

    Create UnderGraduateStudent.cs

---
+++ Project1/Project1/UnderGraduateStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class UnderGraduateStudent:Student
+    {
+        public int Year {  get; set; }
+
+        public UnderGraduateStudent(string name, string major, string id, int year) : base(name, major, id)
+        {
+            this.Year = year;
+        }
+
+        public void DeclareMajor(string major)
+        {
+            Major = major;
+        }
+
+    }
+}

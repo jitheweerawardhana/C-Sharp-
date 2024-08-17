43a3a5bbb935ce05c784bd4fe7199d6f248f4585
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-17 09:17:03 +0530

    Create Singleton.cs

---
+++ Eager_Initialization/Eager_Initialization/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eager_Initialization
+{
+    internal class Singleton
+    {
+        private static Singleton instance = new Singleton();
+
+        private Singleton() 
+        {
+            Console.WriteLine("Singleton is Initialization");
+        }
+
+        public static Singleton GetInstance() {
+            return instance;
+        }
+
+        public void DisplayMsg
+    }
+}

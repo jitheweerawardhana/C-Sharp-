26ac4e869dba21b8af330d4dbc0a312b84c6c84f
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-17 09:17:22 +0530

    Create Program.cs

---
+++ Eager_Initialization/Eager_Initialization/Program.cs
@@ -0,0 +1,20 @@
+﻿using Eager_Initialization;
+using System;
+
+
+namespace SingletonPattern
+{
+    public class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Singleton instance1 = Singleton.GetInstance();
+            Singleton instance2 = Singleton.GetInstance();
+
+            if (instance1 == instance2)
+            {
+                
+
+            }
+        }
+}

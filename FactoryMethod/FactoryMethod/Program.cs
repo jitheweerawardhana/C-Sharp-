5da63c4c07c6703566d368bd5d8a1c2642cff0ab
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-17 09:15:47 +0530

    Create Program.cs

---
+++ FactoryMethod/FactoryMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace FactoryMethod;
+
+public class Program
+{
+    public static void Main(String[]args)
+    {
+        BeefResturent beefResturent = new BeefResturent();
+        beefResturent.OrderBurger();
+
+        VeggiResturent veggiResturent = new VeggiResturent();
+        veggiResturent.OrderBurger();
+
+        ChickenBurger chickenBurger = new ChickenBurger();
+        chickenBurger.OrderBurger();
+    }
+}

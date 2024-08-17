1cd2244591ec5839922565b5ea34f2eaf14cf174
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-17 09:15:36 +0530

    Create VeggiBurger.cs

---
+++ FactoryMethod/FactoryMethod/VeggiBurger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class VeggiBurger : Burger
+    {
+        public override void Prepare()
+        {
+            Console.WriteLine("VeggiBurger");
+        }
+    }
+}

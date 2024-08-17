679aecd690f313e816a6ffcd15a4a7867857253d
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-17 09:15:40 +0530

    Create Resturent.cs

---
+++ FactoryMethod/FactoryMethod/Resturent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public abstract class Resturent
+    {
+        public Burger OrderBurger()
+        {
+            Burger burger = CreateBurger();
+            burger.Prepare();
+            return burger;  
+        }
+
+        public abstract Burger CreateBurger();
+    }
+}

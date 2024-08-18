e4a74f4bf830ee1cb35b0f98c6b7d6bf8ba60bc0
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-18 23:10:53 +0530

    Create Course.cs

---
+++ computerArch/computerArch/Course.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computerArch
+{
+    public class Course
+    {
+        public string Title {  get; set; }
+        public string CourseId { get; set; }
+        public int Credits { get; set; }
+        public List<Student> Students { get; set; }
+        public List<Professor> professors { get; set; }
+
+        public Course(string title,string id, int credits)
+        {
+            Title = title;
+            CourseId = id;
+            Credits = credits;
+            Students = new List<Student>();
+            professors = new List<Professor>();
+        }
+
+        public void EnrollStudent(Student student)
+        {
+            Students.Add(student);
+        }
+
+        public List<Student> GetStudents()
+        {
+
+        return Students; 
+        }
+
+        public void SetProfessor(Professor professor)
+        {
+            professors.Add(professor);
+        }
+
+        public List<Professor> GetProfessors()
+        {
+            return professors;
+        }
+    
+    }
+}

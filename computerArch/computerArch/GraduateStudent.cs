c58707a28f792642f72dd1252771cd45d22b30bf
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-18 23:10:58 +0530

    Create GraduateStudent.cs

---
+++ computerArch/computerArch/GraduateStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computerArch
+{
+    public class GraduateStudent: Student
+    {
+        public string ReaserchTopic { get; set; }
+
+        public GraduateStudent(string name,string major,string id, string topic):base(name, major, id)
+        {
+            ReaserchTopic = topic;
+        }
+
+        public void ApplyForTA()
+        {
+            Console.WriteLine("Apply for ta");
+        }
+    }
+
+}

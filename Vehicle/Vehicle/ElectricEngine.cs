315f57cfdd6e91575e86ad5c9808fadac50e5828
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-22 20:55:02 +0530

    Create ElectricEngine.cs

---
+++ Vehicle/Vehicle/ElectricEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    public class ElectricEngine : IEngine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Starting Electric Engine");
+        }
+    }
+}

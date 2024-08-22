f56a063924ec073f6d47bdc4ba4b440e99b68d4b
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-22 23:20:13 +0530

    Create GasolineEngine.cs

---
+++ Vehicle/Vehicle/GasolineEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    public class GasolineEngine : IEngine
+    {
+        public void Start()
+        {
+            Console.WriteLine("Starting Gasoline Engine");
+        }
+    }
+}

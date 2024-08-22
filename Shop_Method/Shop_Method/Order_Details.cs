926faf55455a7377e2b43340217da5a4b93230c6
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-22 23:21:54 +0530

    Create Order_Details.cs

---
+++ Shop_Method/Shop_Method/Order_Details.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Method
+{
+    public class Order_Details
+    {
+        public int Quantity { get; set; }
+        public string TaxStatus { get; set; }
+        public Item Item { get; set; }
+
+    }
+
+    public float CalcSubTotal()
+    {
+        return Item.GetPriceForQuantity(Quantity);
+    }
+
+    public float CalcWeight()
+    {
+        return Item.ShippingWeight * Quantity;
+    }
+
+    public float CalcTax()
+    {
+        return CalcSubTotal() * (TaxStatus == "Taxable" ? 0.1f : 0);
+    }
+}

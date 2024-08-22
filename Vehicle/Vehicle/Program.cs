95fba49e19870acd6a81cc8d90e348a274446a72
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-22 23:20:55 +0530

    Create Program.cs

---
+++ Vehicle/Vehicle/Program.cs
@@ -0,0 +1,23 @@
+﻿using Vehicle;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IVehicleFactory gasolineFactory = new GasolineVehicleFactory();
+
+        IVehicle gasolineCar = gasolineFactory.CreateCar();
+        IEngine gasolineEngine = gasolineFactory.CreateEngine();
+
+        gasolineCar.ShowDetails();
+        gasolineEngine.Start();
+
+        IVehicleFactory electricFactory = new ElectricVehicleFactory();
+
+        IVehicle electricTruck = electricFactory.CreateTruck();
+        IEngine electricEngine = electricFactory.CreateEngine();
+
+        electricTruck.ShowDetails();
+        electricEngine.Start();
+    }
+}

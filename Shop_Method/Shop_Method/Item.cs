0f8eb9aca546e6ab262a6c5fcc85e9479fe72f47
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-22 23:21:43 +0530

    Create Item.cs

---
+++ Shop_Method/Shop_Method/Item.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Method
+{
+    public class Item
+    {
+        public int ShippingWeight { get; set; }
+        public String Description { get; set; }
+    }
+
+    public float GetPriceForQuantity(int quantity)
+    {
+        
+        return 20.0f * quantity;
+    }
+
+    public float GetTax()
+    {
+        return 0.1f * GetPriceForQuantity(1);
+    }
+
+    public bool InStock()
+    {
+        
+        return true;
+    }
+}

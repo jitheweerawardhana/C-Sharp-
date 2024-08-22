6c7e9e82f0e6278c740194d21c937cf4e34b85a9
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-08-22 23:20:07 +0530

    Create ElectricVehicleFactory.cs

---
+++ Vehicle/Vehicle/ElectricVehicleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    public class ElectricVehicleFactory : IVehicleFactory
+    {
+        public IVehicle CreateCar()
+        {
+            return new ElectricCar();
+        }
+
+        public IVehicle CreateTruck()
+        {
+            return new ElectricTruck();
+        }
+
+        public IEngine CreateEngine()
+        {
+            return new ElectricEngine();
+        }
+    }
+}

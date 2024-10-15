8b90f03903462e2a3a128497355e29c56e320d00
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-10-15 23:20:05 +0530

    Create Credit.cs

---
+++ Shop_Method/Shop_Method/Credit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Method
+{
+    internal class Credit : Payment
+    {
+        private String name { get; set; }
+
+        private String Type { get; set; }
+
+        private DateTime ExpDate { get; set; }
+
+
+    }
+}

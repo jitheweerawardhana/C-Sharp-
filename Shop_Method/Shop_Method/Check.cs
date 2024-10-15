261a5896ce90e050730d5d24014f8cca48637bb4
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-10-15 23:20:12 +0530

    Create Check.cs

---
+++ Shop_Method/Shop_Method/Check.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Method
+{
+    internal class Check : Payment
+    {
+        private String name { get; set; }
+
+        private String BankID { get; set; }
+    }
+
+    public void authorized()
+    {
+
+    }
+}

5e71f9ec44360190e826a5295f41c9b6415dd41c
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2024-10-15 23:22:52 +0530

    Create Program.cs

---
+++ ComputerCompany/ComputerCompany/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ComputerCompany;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MSIManufacturer msiManufacturer = new MSIManufacturer();
+        msiManufacturer.AssembleGpu();
+
+        AsusManufacturer asusManufacturer = new AsusManufacturer();
+        asusManufacturer.AssembleGpu();
+    }
+}

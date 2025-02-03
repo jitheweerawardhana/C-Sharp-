37878c9c5da08e3108d959908381ea1ee652ddb8
Jithendra Weerawardhana <126315871+jitheweerawardhana@users.noreply.github.com>
2025-02-03 22:53:36 +0530

    Create FactrorialFinder.cs

---
+++ 29044/29044/FactrorialFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29044
+{
+    public class FactorialFinder
+    {
+        public long Factorial(int number)
+        {
+            if (number <= 1) return 1;
+
+            return number * Factorial(number - 1);
+        }
+
+        public long IterativeFactorial(int n)
+        {
+            long result = 1;
+            for (long i = n; i >= 1; i--)
+            {
+                result *= i;
+            }
+            return result;
+
+        }
+    }
+}
